426633e0421c292620150f92559506519470c745
Mariya <mariaomware@gmail.com>
2024-05-23 08:48:21 +0300

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+   
+    fibonacci_sequence = []
+    
+   
+    if length <= 0:
+        print(fibonacci_sequence)
+        return
+    
+   
+    for i in range(length):
+        if i == 0:
+            
+            fibonacci_sequence.append(0)
+        elif i == 1:
+            
+            fibonacci_sequence.append(1)
+        else:
+            
+            fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+    
+   
+    print(fibonacci_sequence)
+
+
+print_fibonacci(10)  
+            
